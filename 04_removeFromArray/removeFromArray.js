d1eee8d09cf635ef71b3e07d6664b6eeeb3995c2
soch0788 <118412850+soch0788@users.noreply.github.com>
2023-02-27 22:08:30 -0600

    completion of 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    let inputArray = arguments[0];
+    for (let i = 1; i < arguments.length; i++){
+    let removedValue = arguments[i];
+    if (inputArray.indexOf(removedValue) != -1) {
+        inputArray.splice(inputArray.indexOf(removedValue),1)
+    }
+}
+    return inputArray
 };
 
 // Do not edit below this line
