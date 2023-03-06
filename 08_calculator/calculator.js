e455b473d874668626cc3acc73868d9318a1cb57
soch0788 <118412850+soch0788@users.noreply.github.com>
2023-03-05 21:48:52 -0600

    completion of 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return(num1+num2)
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return(num1-num2)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0
+  for(let i =0; i < array.length; i++)
+  {
+    sum += array[i]
+  }
+  return(sum)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mult = 1
+  for(let i =0; i < array.length; i++)
+  {
+    mult *= array[i]
+  }
+  return(mult)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return(num1**num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let mult = 1
+  for(let i = 1; i<= num; i++)
+  {
+    mult *= i
+  }
+  return(mult)
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (testString) {
+let newString = ""
+let words = testString.split(/\W/)
+testString = words.join("")
+testString = testString.toLowerCase()
+for(i=0;i<testString.length;i++){
+    newString = testString[i] + newString
+}
+return(newString==testString)
 };
 
 // Do not edit below this line
