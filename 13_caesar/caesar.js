3f08fa6387bec61b9e7eaf623b6e9bfffde33296
soch0788 <118412850+soch0788@users.noreply.github.com>
2023-03-06 15:23:11 -0600

    completed 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,34 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+let newString = ""
+const regexTest = /\w/
+for(let i = 0; i < string.length; i++)
+{   
+    let Lcase = 1
+    let temp = string.charCodeAt(i)
+    if(temp > 96)
+    {
+        Lcase = 0
+    }
+    if(regexTest.test(string[i])){
+    temp = temp + shift
+    while(Lcase == 0 && temp > 122){
+        temp -= 26
+    }
+    while(Lcase == 0 && temp < 97){
+        temp += 26
+    }
+    while(Lcase == 1 && temp > 90){
+        temp -= 26
+    }
+    while(Lcase == 1 && temp < 65){
+        temp += 26
+    }
 
+    }
+    
+    newString += String.fromCharCode(temp)
+}
+return(newString)
 };
 
 // Do not edit below this line
